14bafed7dec9c25bacefab4450e100920f869bcc
Reneegupta16 <154832697+Reneegupta16@users.noreply.github.com>
2024-06-25 20:31:53 +0530

    Create Model Classes  3.1. User.java

---
+++ Model Classes  3.1. User.java
@@ -0,0 +1,20 @@
+package com.example.financemanager.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Transaction> transactions;
+}
+
